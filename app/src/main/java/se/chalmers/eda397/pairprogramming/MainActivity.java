173c3c3f058df0933bd1f6d74237a2c17551558c
pukki <sebastian.pukki@gmail.com>
2015-04-20 15:16:35 +0200

    Added connection to GitHub for searching repositories as test.

--- app/src/main/java/se/chalmers/eda397/pairprogramming/MainActivity.java
+++ app/src/main/java/se/chalmers/eda397/pairprogramming/MainActivity.java
@@ -1,26 +1,60 @@
 package se.chalmers.eda397.pairprogramming;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends ActionBarActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private TextView text = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = (TextView)findViewById(R.id.repo_text);
+        final Button button = (Button) findViewById(R.id.repo_search);
+        button.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        EditText input = (EditText)findViewById(R.id.repo_input);
+        String repoName = input.getText().toString();
+
+        String url = "https://api.github.com/search/repositories?q=" + repoName + "+in:name";
+        new RestClient().execute(url);
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        System.out.println("testbranch");
         return true;
     }
 
@@ -37,5 +71,60 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class RestClient extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString;
+
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+
+                    responseString = out.toString();
+
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                responseString = e.getMessage();
+            }
+
+            return responseString;
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                String parsedString = "";
+                JSONObject jResult = new JSONObject(result);
+                JSONArray jArray = jResult.getJSONArray("items");
+
+                for (int i=0; i< jArray.length(); i++) {
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    parsedString = parsedString + System.getProperty("line.separator") + jObject.getInt("id") + ": " + jObject.getString("full_name");
+                }
+
+                text.setText(parsedString);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                text.setText(e.getMessage());
+            }
+
+            super.onPostExecute(result);
+        }
     }
 }
