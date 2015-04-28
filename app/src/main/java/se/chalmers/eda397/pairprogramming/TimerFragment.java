9832a12602238bc899ffa2e8ad55f30cb5b19d1c
markm1992 <markm1992@yahoo.com>
2015-04-28 13:21:35 +0200

    Timer Logic

--- app/src/main/java/se/chalmers/eda397/pairprogramming/TimerFragment.java
+++ app/src/main/java/se/chalmers/eda397/pairprogramming/TimerFragment.java
@@ -1,108 +1,207 @@
 package se.chalmers.eda397.pairprogramming;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.os.CountDownTimer;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Timer;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TimerFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TimerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TimerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
+public class TimerFragment extends Fragment implements View.OnClickListener{
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TimerFragment.
+     * The fragment argument representing the section number for this
+     * fragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static TimerFragment newInstance(String param1, String param2) {
-        TimerFragment fragment = new TimerFragment();
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private View mRootView = null;
+    private CountDownTimer cdtimer;
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static TimerFragment  newInstance(int sectionNumber) {
+        TimerFragment  fragment = new TimerFragment ();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public TimerFragment() {
-        // Required empty public constructor
+    public TimerFragment () {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_timer, container, false);
 
+        final Button startButton = (Button) mRootView.findViewById(R.id.start_button);
+        final Button stopButton = (Button) mRootView.findViewById(R.id.stop_button);
+        final Button pauseButton = (Button) mRootView.findViewById(R.id.pause_button);
+        startButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
+        pauseButton.setOnClickListener(this);
 
+        return mRootView;
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onClick(View v) {
+        EditText input = (EditText)mRootView.findViewById(R.id.timer_input);
+        int time = Integer.parseInt(input.getText().toString());
+        TextView output = (TextView)mRootView.findViewById(R.id.timer_output);
+        if(v.getId() == R.id.start_button) {
+            startTimer(time);
+        }
+        else if(v.getId() == R.id.stop_button) {
+            stopTimer();
+        }
+        else if(v.getId() == R.id.pause_button) {
+            //pauseTimer();
+        }
+        //else if(v.getId() == R.id.resume_button) {
+            //resumeTimer();
+        //}
+        /*if(((Button)v).getText()=="start_timer") {
+            startTimer(time);
+        }
+        else if(((Button)v).getText()=="stop") {
+
+        } if(((Button)v).getText()=="pause") {
+
+        }*/
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    public void startTimer(int time)
+    {
+        mRootView.findViewById(R.id.timer_input).setVisibility(View.GONE);
+        mRootView.findViewById(R.id.timer_output).setVisibility(View.VISIBLE);
+        mRootView.findViewById(R.id.start_button).setVisibility(View.GONE);
+        final TextView output = (TextView)mRootView.findViewById(R.id.timer_output);
+        cdtimer = new CountDownTimer((time*1000), 1000) {
+            int hr;
+            int min;
+            int sec;
+            public void onTick(long millisUntilFinished) {
+                /*h = x / (60*60*1000)
+                x = x - h*(60*60*1000)
+                m = x / (60*1000)
+                x = x - m*(60*1000)
+                s = x / 1000
+                x = x - s*1000*/
+                hr = (int)(millisUntilFinished);
+                millisUntilFinished = ((millisUntilFinished/1000)/60)/60;
+
+                output.setText(hr+":" + min+":"+sec);
+            }
+
+            public void onFinish() {
+                output.setText("Done!");
+                mRootView.findViewById(R.id.timer_output).setVisibility(View.GONE);
+                mRootView.findViewById(R.id.timer_input).setVisibility(View.VISIBLE);
+                mRootView.findViewById(R.id.start_button).setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+    }
+
+    public void stopTimer()
+    {
+        cdtimer.cancel();
+        mRootView.findViewById(R.id.timer_output).setVisibility(View.GONE);
+        mRootView.findViewById(R.id.timer_input).setVisibility(View.VISIBLE);
+        mRootView.findViewById(R.id.start_button).setVisibility(View.VISIBLE);
+    }
+
+    public void pauseTimer()
+    {
+        //cdtimer.
+    }
+
+    private class RestClient extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString;
+
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+
+                    responseString = out.toString();
+
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                responseString = e.getMessage();
+            }
+
+            return responseString;
+        }
+
+        protected void onPostExecute(String result) {
+            TextView text = (TextView)mRootView.findViewById(R.id.repo_text);
+
+            try {
+                String parsedString = "";
+                JSONObject jResult = new JSONObject(result);
+                JSONArray jArray = jResult.getJSONArray("items");
+
+                for (int i=0; i< jArray.length(); i++) {
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    parsedString = parsedString + System.getProperty("line.separator") + jObject.getInt("id") + ": " + jObject.getString("full_name");
+                }
+
+                text.setText(parsedString);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                text.setText(e.getMessage());
+            }
+
+            super.onPostExecute(result);
+        }
     }
 
 }
