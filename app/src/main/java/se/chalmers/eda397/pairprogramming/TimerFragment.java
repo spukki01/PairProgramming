f980993ddb3017fbb8e84605992c0f97542c59b1
GraceBarathy <grace.barathy@gmail.com>
2015-05-06 12:22:06 +0200

    Added validation for the Timer

--- app/src/main/java/se/chalmers/eda397/pairprogramming/TimerFragment.java
+++ app/src/main/java/se/chalmers/eda397/pairprogramming/TimerFragment.java
@@ -13,6 +13,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.IllegalFormatException;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -28,10 +32,11 @@ import org.w3c.dom.Text;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.sql.Time;
+import java.util.IllegalFormatException;
 import java.util.Timer;
 
 
-public class TimerFragment extends Fragment implements View.OnClickListener{
+public class TimerFragment extends Fragment implements View.OnClickListener {
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -41,19 +46,20 @@ public class TimerFragment extends Fragment implements View.OnClickListener{
     private View mRootView = null;
     private CountDownTimer cdtimer;
     private long PauseTime;
+
     /**
      * Returns a new instance of this fragment for the given section
      * number.
      */
-    public static TimerFragment  newInstance(int sectionNumber) {
-        TimerFragment  fragment = new TimerFragment ();
+    public static TimerFragment newInstance(int sectionNumber) {
+        TimerFragment fragment = new TimerFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public TimerFragment () {
+    public TimerFragment() {
     }
 
     @Override
@@ -80,75 +86,76 @@ public class TimerFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        EditText input = (EditText)mRootView.findViewById(R.id.timer_input);
+        EditText input = (EditText) mRootView.findViewById(R.id.timer_input);
         String inputStr = input.getText().toString();
-        TextView output = (TextView)mRootView.findViewById(R.id.timer_output);
-        int min= 0, hr = 0, sec = 0;
-        if(inputStr.lastIndexOf(":")>=0){
-            sec = Integer.parseInt(inputStr.substring(inputStr.lastIndexOf(":")+1, inputStr.length()));
-            inputStr = inputStr.substring(0,inputStr.lastIndexOf(":"));
-            if(inputStr.lastIndexOf(":")>=0){
-                min = Integer.parseInt(inputStr.substring(inputStr.lastIndexOf(":")+1, inputStr.length()));
-                inputStr = inputStr.substring(0,inputStr.lastIndexOf(":"));
-                if(inputStr.lastIndexOf(":")>=0) {
-                    hr = Integer.parseInt(inputStr.substring(inputStr.lastIndexOf(":")+1, inputStr.length()));
-                }
-                else {
+        TextView output = (TextView) mRootView.findViewById(R.id.timer_output);
+        int min = 0, hr = 0, sec = 0;
+        if (inputStr.lastIndexOf(":") >= 0) {
+            sec = Integer.parseInt(inputStr.substring(inputStr.lastIndexOf(":") + 1, inputStr.length()));
+            inputStr = inputStr.substring(0, inputStr.lastIndexOf(":"));
+            if (inputStr.lastIndexOf(":") >= 0) {
+                min = Integer.parseInt(inputStr.substring(inputStr.lastIndexOf(":") + 1, inputStr.length()));
+                inputStr = inputStr.substring(0, inputStr.lastIndexOf(":"));
+                if (inputStr.lastIndexOf(":") >= 0) {
+                    hr = Integer.parseInt(inputStr.substring(inputStr.lastIndexOf(":") + 1, inputStr.length()));
+                } else {
                     hr = Integer.parseInt(inputStr);
                 }
-            }
-            else {
+            } else {
                 min = Integer.parseInt(inputStr);
             }
-        }
-        else {
+        } else {
             sec = Integer.parseInt(inputStr);
         }
+        if (hr > 24 || min > 60 || sec > 60) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Invalid input Enter correct time and correct format HH:MM:SS.")
+                    .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                           TimerFragment.this.stopTimer();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            AlertDialog alert = builder.create();
+            alert.show();
 
-        int time = (hr * 60*60) + (min * 60) + sec;
-
-        /**if (time > 216000){
-            System.out.println("Error: Should not exceed 216000 seconds");
-             return null ;
-        }*/
-
-        if(v.getId() == R.id.start_button) {
-            startTimer(time);
         }
-        else if(v.getId() == R.id.stop_button) {
+
+        int time = (hr * 60 * 60) + (min * 60) + sec;
+
+        if (v.getId() == R.id.start_button) {
+                startTimer(time);
+        } else if (v.getId() == R.id.stop_button) {
             stopTimer();
-        }
-        else if(v.getId() == R.id.pause_button) {
+        } else if (v.getId() == R.id.pause_button) {
             pauseTimer();
-        }
-        else if(v.getId() == R.id.resume_button) {
+        } else if (v.getId() == R.id.resume_button) {
             resumeTimer();
         }
 
     }
 
-    public void startTimer(int time)
-    {
+    public void startTimer(int time)  {
         mRootView.findViewById(R.id.timer_input).setVisibility(View.GONE);
         mRootView.findViewById(R.id.timer_output).setVisibility(View.VISIBLE);
         mRootView.findViewById(R.id.start_button).setVisibility(View.GONE);
-        final TextView output = (TextView)mRootView.findViewById(R.id.timer_output);
-        cdtimer = new CountDownTimer((time*1000), 1000) {
+        final TextView output = (TextView) mRootView.findViewById(R.id.timer_output);
+        cdtimer = new CountDownTimer((time * 1000), 1000) {
             int h;
             int m;
             int s;
-            public void onTick(long x){
+
+            public void onTick(long x) {
+
                 PauseTime = x;
-                h = (int)(x / (60*60*1000));
-                x = (x - h*(60*60*1000));
-                m = (int)(x / (60*1000));
-                x = (x - m*(60*1000));
-                s = (int)(x / 1000);
-                x = (x - s*1000);
-                /**if (h > 24 || m > 60 || s > 60 ) {
-                    throw new Exception("Bad input please enter correct time with : separator");
-                }**/
-                String outputString = String.format("%02d:%02d:%02d", h,m,s);
+                h = (int) (x / (60 * 60 * 1000));
+                x = (x - h * (60 * 60 * 1000));
+                m = (int) (x / (60 * 1000));
+                x = (x - m * (60 * 1000));
+                s = (int) (x / 1000);
+                x = (x - s * 1000);
+
+                String outputString = String.format("%02d:%02d:%02d", h, m, s);
                 output.setText(outputString);
             }
 
@@ -157,50 +164,45 @@ public class TimerFragment extends Fragment implements View.OnClickListener{
                 mRootView.findViewById(R.id.timer_output).setVisibility(View.GONE);
                 mRootView.findViewById(R.id.timer_input).setVisibility(View.VISIBLE);
                 mRootView.findViewById(R.id.start_button).setVisibility(View.VISIBLE);
-
             }
         }.start();
     }
 
-    public void stopTimer()
-    {
+    public void stopTimer() {
         cdtimer.cancel();
         mRootView.findViewById(R.id.timer_output).setVisibility(View.GONE);
         mRootView.findViewById(R.id.timer_input).setVisibility(View.VISIBLE);
         mRootView.findViewById(R.id.start_button).setVisibility(View.VISIBLE);
     }
 
-    public void pauseTimer()
-    {
-       cdtimer.cancel();
+    public void pauseTimer() {
+        cdtimer.cancel();
         mRootView.findViewById(R.id.pause_button).setVisibility(View.GONE);
         mRootView.findViewById(R.id.resume_button).setVisibility(View.VISIBLE);
-
-
     }
 
     public void resumeTimer()
 
     {
-        final TextView output = (TextView)mRootView.findViewById(R.id.timer_output);
+        final TextView output = (TextView) mRootView.findViewById(R.id.timer_output);
         mRootView.findViewById(R.id.pause_button).setVisibility(View.VISIBLE);
         mRootView.findViewById(R.id.resume_button).setVisibility(View.GONE);
 
-        cdtimer = new CountDownTimer(PauseTime,1000) {
+        cdtimer = new CountDownTimer(PauseTime, 1000) {
             int h;
             int m;
             int s;
+
             public void onTick(long x) {
                 PauseTime = x;
-                h = (int)(x / (60*60*1000));
-                x = (x - h*(60*60*1000));
-                m = (int)(x / (60*1000));
-                x = (x - m*(60*1000));
-                s = (int)(x / 1000);
-                x = (x - s*1000);
-                String outputString = String.format("%02d:%02d:%02d", h,m,s);
+                h = (int) (x / (60 * 60 * 1000));
+                x = (x - h * (60 * 60 * 1000));
+                m = (int) (x / (60 * 1000));
+                x = (x - m * (60 * 1000));
+                s = (int) (x / 1000);
+                x = (x - s * 1000);
+                String outputString = String.format("%02d:%02d:%02d", h, m, s);
                 output.setText(outputString);
-
             }
 
             public void onFinish() {
@@ -213,7 +215,7 @@ public class TimerFragment extends Fragment implements View.OnClickListener{
         }.start();
     }
 
-       private class RestClient extends AsyncTask<String, String, String> {
+    private class RestClient extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... uri) {
@@ -225,14 +227,14 @@ public class TimerFragment extends Fragment implements View.OnClickListener{
                 response = httpclient.execute(new HttpGet(uri[0]));
                 StatusLine statusLine = response.getStatusLine();
 
-                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
                     response.getEntity().writeTo(out);
 
                     responseString = out.toString();
 
                     out.close();
-                } else{
+                } else {
                     //Closes the connection.
                     response.getEntity().getContent().close();
                     throw new IOException(statusLine.getReasonPhrase());
@@ -247,14 +249,14 @@ public class TimerFragment extends Fragment implements View.OnClickListener{
         }
 
         protected void onPostExecute(String result) {
-            TextView text = (TextView)mRootView.findViewById(R.id.repo_text);
+            TextView text = (TextView) mRootView.findViewById(R.id.repo_text);
 
             try {
                 String parsedString = "";
                 JSONObject jResult = new JSONObject(result);
                 JSONArray jArray = jResult.getJSONArray("items");
 
-                for (int i=0; i< jArray.length(); i++) {
+                for (int i = 0; i < jArray.length(); i++) {
                     JSONObject jObject = jArray.getJSONObject(i);
                     parsedString = parsedString + System.getProperty("line.separator") + jObject.getInt("id") + ": " + jObject.getString("full_name");
                 }
