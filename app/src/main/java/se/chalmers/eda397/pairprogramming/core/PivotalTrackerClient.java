bd6dd4e0ba2410131af367fcc88841db5cfd966a
pukki <sebastian.pukki@gmail.com>
2015-05-12 18:37:34 +0200

    Fetch US from pivotal tracker #PT1314922:93184114

--- app/src/main/java/se/chalmers/eda397/pairprogramming/core/PivotalTrackerClient.java
+++ app/src/main/java/se/chalmers/eda397/pairprogramming/core/PivotalTrackerClient.java
@@ -1,9 +1,36 @@
 package se.chalmers.eda397.pairprogramming.core;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import se.chalmers.eda397.pairprogramming.model.UserStory;
+import se.chalmers.eda397.pairprogramming.util.IMapper;
+import se.chalmers.eda397.pairprogramming.util.UserStoryMapper;
+
 public class PivotalTrackerClient implements IPivotalTrackerClient {
+
     private IConnectionManager mConnectionManager;
+    private IMapper<UserStory> mMapper;
 
     public PivotalTrackerClient(IConnectionManager connectionManager) {
         this.mConnectionManager = connectionManager;
+        this.mMapper = new UserStoryMapper();
+    }
+
+    @Override
+    public UserStory fetchUserStory(int projectId, int storyId) {
+        String find_repo_url = "https://www.pivotaltracker.com/services/v5/projects/"+projectId+"/stories/"+storyId;
+        String response = this.mConnectionManager.executeQuery(find_repo_url);
+
+        UserStory us = null;
+        try {
+            JSONObject jResult = new JSONObject(response);
+            us = mMapper.map(jResult);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return us;
     }
 }
